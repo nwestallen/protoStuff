137afe915a0250098a5dc3efe91f2f1d0555a910
Nick <nwestallen@gmail.com>
2023-03-27 18:54:04 -0400

    removes recursive versions, adds tests

--- bench.js
+++ bench.js
@@ -1,32 +1,16 @@
-const Benchmark = require('benchmark')
-const { parseBIG, parse32, parseBIGWhile, parse32While } = require("./protobuffParse")
+const Benchmark = require("benchmark");
+const { parseBIG, parse32 } = require("./protobuffParse");
 
-const suite12345 = new Benchmark.Suite('Basic Test')
+const suite12345 = new Benchmark.Suite("Basic Test");
 const countUp = new Uint8Array([0xc0, 0xc4, 0x07, 0x96, 0x01]);
 
 suite12345
-    
-    .add('parseBIG', () => parseBIG(countUp))
-    .add('parse32', () => parse32(countUp))
-    .add('paresBIGWhile', () => parseBIGWhile(countUp))
-    .add('parse32While', () => parse32While(countUp))
-    .on('start', event => console.log('---------12345 Test Suite------------\n'))
-    .on('cycle', event => console.log(String(event.target)))
-    .on('complete', event => {
-    const suite = event.currentTarget;
-    const fastestOption = suite.filter('fastest').map('name')
-    console.log(`\nThe fastest option is ${fastestOption}\n`) })
-    .run();
 
-const suiteMaxNeg = new Benchmark.Suite('Max Negative Test')
-const maxNegSigned = new Uint8Array([0x80, 0x80, 0x80, 0x80, 0x08]);
-
-suiteMaxNeg
-  .add("parseBIG", () => parseBIG(maxNegSigned))
-  .add("parse32", () => parse32(maxNegSigned))
-  .add("paresBIGWhile", () => parseBIGWhile(maxNegSigned))
-  .add("parse32While", () => parse32While(maxNegSigned))
-  .on("start", (event) => console.log("---------Max Negative Test Suite------------\n"))
+  .add("parseBIG", () => parseBIG(countUp))
+  .add("parse32", () => parse32(countUp))
+  .on("start", (event) =>
+    console.log("---------12345 Test Suite------------\n")
+  )
   .on("cycle", (event) => console.log(String(event.target)))
   .on("complete", (event) => {
     const suite = event.currentTarget;
@@ -35,41 +19,52 @@ suiteMaxNeg
   })
   .run();
 
-  const suiteString = new Benchmark.Suite();
+const suiteMaxNeg = new Benchmark.Suite("Max Negative Test");
+const maxNegSigned = new Uint8Array([0x80, 0x80, 0x80, 0x80, 0x08]);
 
-  // add tests
-  suiteString
-    .add("RegExp#test", function () {
-      /o/.test("Hello World!");
-    })
-    .add("String#indexOf", function () {
-      "Hello World!".indexOf("o") > -1;
-    })
-    .add("String#match", function () {
-      !!"Hello World!".match(/o/);
-    })
-    // add listeners
-    .on("cycle", function (event) {
-      console.log(String(event.target));
-    })
-    .on("complete", function () {
-      console.log("\nFastest is " + this.filter("fastest").map("name"));
-    })
-    // run async
-    .run({ async: true });
+suiteMaxNeg
+  .add("parseBIG", () => parseBIG(maxNegSigned))
+  .add("parse32", () => parse32(maxNegSigned))
+  .on("start", (event) =>
+    console.log("---------Max Negative Test Suite------------\n")
+  )
+  .on("cycle", (event) => console.log(String(event.target)))
+  .on("complete", (event) => {
+    const suite = event.currentTarget;
+    const fastestOption = suite.filter("fastest").map("name");
+    console.log(`\nThe fastest option is ${fastestOption}\n`);
+  })
+  .run();
 
-    const suiteNumbers = new Benchmark.Suite();
-    const normalNum = 1992
-    const bigNum = BigInt(1992)
+const suiteString = new Benchmark.Suite();
 
-    suiteNumbers
-      .add("Normal Num", () => normalNum ** 2)
-      .add("Big Num", () => bigNum ** 2n)
-      .on("start", () => console.log("-----------Numbers Test--------------"))
-      .on("cycle", (event) => console.log(String(event.target)))
-      .on("complete", (event) => {
-        const suite = event.currentTarget;
-        const fastestOption = suite.filter("fastest").map("name");
-        console.log(`\nThe fastest option is ${fastestOption}\n`);
-      })
-      .run();
+const suiteNumbers = new Benchmark.Suite();
+const normalNum = 1992;
+const bigNum = BigInt(1992);
+
+suiteNumbers
+  .add("Normal Num", () => normalNum ** 2)
+  .add("Big Num", () => bigNum ** 2n)
+  .on("start", () => console.log("-----------Numbers Test--------------"))
+  .on("cycle", (event) => console.log(String(event.target)))
+  .on("complete", (event) => {
+    const suite = event.currentTarget;
+    const fastestOption = suite.filter("fastest").map("name");
+    console.log(`\nThe fastest option is ${fastestOption}\n`);
+  })
+  .run();
+
+const suiteNegative = new Benchmark.Suite();
+
+suiteNegative
+  .add("Bitwise", () => ~150)
+  .add("Based", () => 2 ** 32 - 150)
+  .add("Subtraction", () => 4294967146 - 150)
+  .on("start", () => console.log("-------------Negative Test-----------------"))
+  .on("cycle", (event) => console.log(String(event.target)))
+  .on("complete", (event) => {
+    const suite = event.currentTarget;
+    const fastestOption = suite.filter("fastest").map("name");
+    console.log(`\nThe fastest option is ${fastestOption}\n`);
+  })
+  .run();
--- protobuffParse.js
+++ protobuffParse.js
@@ -3,41 +3,12 @@ const convertBIG = (arr) =>
     .map((byte) => BigInt(byte & 0x7f))
     .reduce((accum, curr, ind) => accum + curr * 128n ** BigInt(ind));
 
-const parseBIG = (arr) => {
-  const recur = (curr, ind) => {
-    if (!(curr[ind] >> 7)) {
-      return convertBIG(curr);
-    } else {
-      curr.push(arr[ind + 1]);
-      return recur(curr, ind + 1);
-    }
-  };
-  return recur([arr[0]], 0);
-};
-
 const convert32 = (arr) =>
   arr
     .map((byte) => byte & 0x7f)
     .reduce((accum, curr, ind) => accum + curr * 128 ** ind);
 
-const parse32 = (arr) => {
-  const recur = (curr, ind) => {
-    if (!(curr[ind] >> 7)) {
-      return curr;
-    } else {
-      curr.push(arr[ind + 1]);
-      return recur(curr, ind + 1);
-    }
-  };
-  const result = recur([arr[0]], 0);
-  if (result[4] > 7) {
-    return -(~convert32(result) + 1);
-  } else {
-    return convert32(result);
-  }
-};
-
-const parseBIGWhile = (arr) => {
+const parseBIG = (arr) => {
   let ind = 0;
   const curr = [arr[0]];
   while (curr[ind] >> 7) {
@@ -47,7 +18,7 @@ const parseBIGWhile = (arr) => {
   return convertBIG(curr);
 };
 
-const parse32While = (arr) => {
+const parse32 = (arr) => {
   let ind = 0;
   const curr = [arr[0]];
   while (curr[ind] >> 7) {
@@ -61,4 +32,4 @@ const parse32While = (arr) => {
   }
 };
 
-module.exports = { parseBIG, parse32, parseBIGWhile, parse32While };
+module.exports = { parseBIG, parse32 };
